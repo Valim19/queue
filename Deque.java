390dbdfe0aacdf140dc5bd09e5b7a12c99e85b94
Anna Valim <annacarolinavalim@gmail.com>
2024-10-03 11:37:03 -0300

    adicionando os arquivos

--- Deque.java
+++ Deque.java
@@ -1,4 +1,14 @@
+/*
+ * To loop through a collection, use the hasNext() and next() methods
+ * Iterators are designed to easily change the collections that they loop through.
+ * The remove() method can remove items from a collection while looping.
+ */
+
+import java.lang.*;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Deque <Item> implements Iterable<Item> {
 
