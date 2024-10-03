c8a6ca0b0d2933cb3405beff3c9f0d8f2b8a1974
Anna Valim <annacarolinavalim@gmail.com>
2024-10-03 11:15:35 -0300

    adicionando os arquivos

---
+++ Deque.java
@@ -0,0 +1,45 @@
+import java.util.Iterator;
+
+public class Deque <Item> implements Iterable<Item> {
+
+    // construct an empty deque
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    // returns the number of itens in the deque
+    public int size() {
+
+    }
+
+    public void addFirst(Item item) {
+
+    }
+
+    public void addLast(Item item) {
+
+    }
+
+    // important to remember it REMOVES and RETURNS item from the front
+    public Item removeFirst() {
+
+    }
+
+    // important to remember it REMOVES and RETURNS item from the back
+    public Item removeLast(){
+
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ RandomizedQueue.java
@@ -0,0 +1,38 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    public RandomizedQueue() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size(){
+
+    }
+
+    public void enqueue(Item item) {
+
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+
+    }
+
+    public Iterator<Item> iterator() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
