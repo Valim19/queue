d673895d10c7b46a0c66c2c350f90ff489cda4c6
Anna Valim <annacarolinavalim@gmail.com>
2024-10-06 17:42:42 -0300

    alterações em RandomizedQueue e Permutation

--- Deque.java
+++ Deque.java
@@ -1,97 +1,159 @@
-/*
- * To loop through a collection, use the hasNext() and next() methods
- * Iterators are designed to easily change the collections that they loop through.
- * The remove() method can remove items from a collection while looping.
- */
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Deque <Item> implements Iterable<Item> {
+public class Deque <Item> implements Iterable <Item> {
     private Node first;
     private Node last;
-    private int counter;
+    private int  counter;
 
     public Deque() {
-        this.first = null;
-        this.last = null;
+        this.first   = null;
+        this.last    = null;
         this.counter = 0;
+    }
+
+    public int size() {
+        return counter;
     }
 
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    // returns the number of itens in the deque
-    public int size() {
-        return counter;
-    }
-
     public void addFirst(Item item) {
-        if (item == null) throw new IllegalArgumentException("Item cannot be a null element");
+        validateNotNull(item);
 
-        Node oldFirst = first;
+        Node oldFirst  = first;
 
-        first = new Node();
-        first.item = item;
-        first.next = oldFirst;
+        first          = new Node();
+        first.item     = item;
+        first.next     = oldFirst;
         first.previous = null;
 
-        if (isEmpty()) last = first;
-        else oldFirst.previous = last;
+        if (isEmpty()) { last    = first; }
+        else { oldFirst.previous = last; }
 
         counter++;
     }
 
     public void addLast(Item item) {
-        if(item == null) throw new IllegalArgumentException("Item cannot be a null element");
+        validateNotNull(item);
 
-        Node oldLast = last;
+        Node oldLast  = last;
 
-        last = new Node();
-        last.item = item;
-        last.next = null;
+        last          = new Node();
+        last.item     = item;
+        last.next     = null;
         last.previous = oldLast;
 
-        if (isEmpty()) first = last;
-        else oldLast.next = last;
+        if (isEmpty()) { first = last; }
+        else { oldLast.next    = last; }
 
         counter++;
     }
 
     public Item removeFirst() {
-        if (isEmpty()) throw new NoSuchElementException("You cannot remove elements from an empty deque");
+        validateNotEmpty();
 
         Node removedFirst = first;
-        first = removedFirst.next;
+        first             = removedFirst.next;
         counter--;
 
-        if (isEmpty()) last = null;
-        else first.previous = null;
+        if (isEmpty()) { last = null; }
+        else { first.previous = null; }
 
         return removedFirst.item;
     }
 
     public Item removeLast(){
-        if (isEmpty()) throw new NoSuchElementException("You cannot remove elements from an empty deque");
+        validateNotEmpty();
 
         Node removedLast = last;
-        last = removedLast.previous;
+        last             = removedLast.previous;
         counter--;
 
-        if(isEmpty()) first = null;
-        else last.next = null;
+        if(isEmpty()) { first = null; }
+        else { last.next      = null; }
 
         return removedLast.item;
     }
 
-    // return an iterator over items in order from front to back
     public Iterator<Item> iterator(){
         return new DequeIterator();
     }
-
     public static void main(String[] args) {
+        Deque<Integer> deck = new Deque<Integer>();
+        
+        testIsEmpty(deck);
+        testAddFirst(deck);
+        testRemoveLast(deck);
+        testAddLast(deck);
+        testRemoveFirst(deck);
+    }
 
+    private void validateNotNull(Item item) {
+        if (item == null) throw new IllegalArgumentException("Item cannot be a null element");
+    }
+
+    private void validateNotEmpty() {
+        if (isEmpty()) throw new NoSuchElementException("You cannot remove from an empty deck");
+    }
+
+    private static void testIsEmpty(Deque<Integer> deck) {
+        assert deck.isEmpty() : "Deck should be initially empty";
+        System.out.println("testIsEmpty passed.");
+    }
+
+    private static void testAddFirst(Deque<Integer> deck) {
+        for (int i = 0; i < 10; i++) {
+            deck.addFirst(i);
+            assert deck.size() == i + 1 : "Size should increase as elements are added";
+            assert !deck.isEmpty() : "Deck should not be empty after adding elements";
+        }
+
+        int expected = 9;
+        for (Integer i : deck) {
+            assert i == expected-- : "Elements should appear from 9 to 0";
+        }
+
+        System.out.println("testAddFirst passed.");
+    }
+
+    private static void testRemoveLast(Deque<Integer> deck) {
+        for (int i = 0; i < 10; i++) {
+            int removed = deck.removeLast();
+            assert removed == i : "Elements should be removed from 0 to 9";
+            assert deck.size() == 9 - i : "Size should decrease as elements are removed";
+        }
+
+        assert deck.isEmpty() : "Deck should be empty after removing all elements";
+        System.out.println("testRemoveLast passed.");
+    }
+
+    private static void testAddLast(Deque<Integer> deck) {
+        for (int i = 0; i < 10; i++) {
+            deck.addLast(i);
+            assert deck.size() == i + 1 : "Size should increase as elements are added";
+            assert !deck.isEmpty() : "Deck should not be empty after adding elements";
+        }
+        
+        int expected = 0;
+        for (Integer i : deck) {
+            assert i == expected++ : "Elements should appear from 0 to 9";
+        }
+
+        System.out.println("testAddLast passed.");
+    }
+
+    private static void testRemoveFirst(Deque<Integer> deck) {
+        for (int i = 0; i < 10; i++) {
+            int removed = deck.removeFirst();
+            assert removed == i : "Elements should be removed from 0 to 9";
+            assert deck.size() == 9 - i : "Size should decrease as elements are removed";
+        }
+
+        assert deck.isEmpty() : "Deck should be empty after removing all elements";
+        System.out.println("testRemoveFirst passed.");
     }
 
     private class Node {
@@ -107,8 +169,8 @@ public class Deque <Item> implements Iterable<Item> {
         }
 
         Node(Item item, Node next, Node previous) {
-            this.item = item;
-            this.next = next;
+            this.item     = item;
+            this.next     = next;
             this.previous = previous;
         }
     }
@@ -120,7 +182,7 @@ public class Deque <Item> implements Iterable<Item> {
             if (!hasNext()) throw new NoSuchElementException();
 
             Item item = current.item;
-            current = current.next;
+            current   = current.next;
 
             return item;
         }
--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,9 +1,3 @@
-/*
- * To loop through a collection, use the hasNext() and next() methods
- * Iterators are designed to easily change the collections that they loop through.
- * The remove() method can remove items from a collection while looping.
- */
-
 import java.lang.*;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -11,39 +5,193 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
+    private int arraySize;
+    private Item[] elements;
+    private int elementsCounter;
 
     public RandomizedQueue() {
-
-    }
-
-    public boolean isEmpty() {
-
+        this.arraySize       = 1;
+        this.elements        = (Item[]) new Object[arraySize];
+        this.elementsCounter = 0;
     }
 
     public int size(){
+        return elementsCounter;
+    }
 
+    public boolean isEmpty() {
+        return size() == 0;
     }
 
     public void enqueue(Item item) {
+        validateNotNull(item);
 
+        if (isFull()) duplicateSize();
+
+        elements[size()] = item;
+        elementsCounter++;
     }
 
-    // remove and return a random item
     public Item dequeue() {
+        validateNotEmpty();
 
+        Item itemToReturn = removeRandomItemAndReorder();
+
+        if (isAlmostEmpty()) halfSize();
+
+        return itemToReturn;
     }
 
-    // return a random item (but do not remove it)
     public Item sample() {
+        validateNotEmpty();
 
+        return elements[randomIndex()];
     }
 
     public Iterator<Item> iterator() {
-
+        return new RandomizedQueueIterator();
+    }
+    public static void main(String[] args) {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+    
+        testIsEmpty(queue);
+        testEnqueue(queue);
+        testDequeue(queue);
+        testSample(queue);
+        testIterator(queue);
+    }
+    
+    private void validateNotNull(Item item) {
+        if (item == null) throw new IllegalArgumentException("Item cannot be a null element");
     }
 
-    public static void main(String[] args) {
+    private void validateNotEmpty() {
+        if (isEmpty()) throw new NoSuchElementException("It cannot bem an empty deck");
+    }
 
+    private void resize(int capacity) {
+        if (isEmpty()) return;
+
+        arraySize = capacity;
+        Item[] copy = (Item[]) new Object[arraySize];
+
+        int maxSize = size();
+        for (int i = 0; i < maxSize; i++) copy[i] = elements[i];
+
+        elements = copy;
+    }
+
+    private void halfSize() {
+        resize(arraySize / 2);
+    }
+
+    private void duplicateSize() {
+        resize(2 * arraySize);
+    }
+
+    private static void testIsEmpty(RandomizedQueue<Integer> queue) {
+        assert queue.isEmpty() : "Queue should be empty initially";
+        assert queue.size() == 0 : "Size should be 0 when the queue is empty";
+        System.out.println("testIsEmpty passed.");
+    }
+    
+    private static void testEnqueue(RandomizedQueue<Integer> queue) {
+        for (int i = 0; i < 5; i++) {
+            queue.enqueue(i);
+            assert queue.size() == i + 1 : "Size should increase as elements are enqueued";
+            assert !queue.isEmpty() : "Queue should not be empty after enqueuing elements";
+        }
+        System.out.println("testEnqueue passed.");
+    }
+    
+    private static void testDequeue(RandomizedQueue<Integer> queue) {
+        int initialSize = queue.size();
+        int elementsRemoved = 0;
+    
+        for (int i = 0; i < initialSize; i++) {
+            Integer removed = queue.dequeue();
+            assert removed != null : "Dequeued element should not be null";
+            elementsRemoved++;
+            assert queue.size() == initialSize - elementsRemoved : "Size should decrease after dequeue";
+        }
+    
+        assert queue.isEmpty() : "Queue should be empty after removing all elements";
+        System.out.println("testDequeue passed.");
+    }
+    
+    private static void testSample(RandomizedQueue<Integer> queue) {
+        for (int i = 0; i < 5; i++) {
+            queue.enqueue(i);
+        }
+    
+        Integer sampled = queue.sample();
+        assert sampled != null : "Sampled element should not be null";
+        assert queue.size() == 5 : "Sampling should not remove elements from the queue";
+        System.out.println("testSample passed.");
+    }
+    
+    private static void testIterator(RandomizedQueue<Integer> queue) {
+        int initialSize = queue.size();
+        int count = 0;
+    
+        for (Integer i : queue) {
+            assert i != null : "Iterator should return non-null elements";
+            count++;
+        }
+    
+        assert count == initialSize : "Iterator should iterate over all elements";
+        assert queue.size() == initialSize : "Iterator should not modify the size of the queue";
+        System.out.println("testIterator passed.");
+    }
+
+    private boolean isFull() {
+        return size() == arraySize;
+    }
+
+    private boolean isAlmostEmpty() {
+        return size() <= arraySize / 4;
+    }
+
+    private int randomIndex() {
+        return StdRandom.uniform(size());
+    }
+
+    private Item removeRandomItemAndReorder() {
+        int index = randomIndex();
+        int lastIndex = size() - 1;
+
+        Item item = elements[index];
+        elements[index] = elements[lastIndex];
+        elements[lastIndex] = null;
+
+        elementsCounter--;
+
+        return item;
+    }
+
+    private class RandomizedQueueIterator {
+        private final RandomizedQueue<Item> copy;
+
+        RandomizedQueueIterator() {
+            copy = new RandomizedQueue<Item>();
+
+            for (int i = 0; i < size(); i++) {
+                copy.enqueue(elements[i]);
+            }
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new java.util.NoSuchElementException("There are no more elements");
+            
+            return copy.dequeue();
+        }
+
+        public boolean hasNext() {
+            return copy.size() > 0;
+        }
+        
+        public void remove() {
+            throw new UnsupportedOperationException("This operation is not supported by this class");
+        }
     }
 }
